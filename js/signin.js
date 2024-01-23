3f8a29bb03c693f74d64ea3507ea8a7659dc7b3b
JeffreyLee <s10258143@connect.np.edu.sg>
2024-01-24 00:51:10 +0800

    Getting Sign In User's Information

--- js/signin.js
+++ js/signin.js
@@ -1,14 +1,12 @@
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+window.handleCredentialResponse = (response) => {
+  // decodeJwtResponse() is a custom function defined by you
+  // to decode the credential response.
+  //   responsePayload = decodeJwtResponse(response.credential);
 
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log("User signed out.");
-  });
-}
+  console.log("ID: " + responsePayload.sub);
+  console.log("Full Name: " + responsePayload.name);
+  console.log("Given Name: " + responsePayload.given_name);
+  console.log("Family Name: " + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
+};
