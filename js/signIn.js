ea9417c41c0ed49d9cf4ba7bea1af775a08c6d71
JeffreyLee <s10258143@connect.np.edu.sg>
2024-01-24 07:04:20 +0800

    POST account information to RestDB after Signing in to Google Account

--- js/signIn.js
+++ js/signIn.js
@@ -1,3 +1,5 @@
+const APIKEY = "58c028f9c481812dde4624100ed946691f4e5";
+
 globalThis.handleCredentialResponse = async (response) => {
   // decodeJwtResponse() is a custom function defined by you
   // to decode the credential response.
@@ -9,7 +11,44 @@ globalThis.handleCredentialResponse = async (response) => {
   console.log("Family Name: " + responsePayload.family_name);
   console.log("Image URL: " + responsePayload.picture);
   console.log("Email: " + responsePayload.email);
+
+  // Storing New Google Account Information
+  let customerId = responsePayload.sub;
+  let customerEmail = responsePayload.email;
+  let customerFirstName = responsePayload.given_name;
+  let customerLastName = responsePayload.family_name;
+  let customerImageURL = responsePayload.picture;
+
+  // Storing Information as a JSON
+  let jsondata = {
+    customerId: customerId,
+    email: customerEmail,
+    firstName: customerFirstName,
+    lastName: customerLastName,
+    imageUrl: customerImageURL,
+  };
+
+  // Create AJAX settings
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://velocity-554e.restdb.io/rest/customer",
+    method: "POST", //POST Request
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache",
+    },
+    processData: false,
+    data: JSON.stringify(jsondata),
+  };
+
+  //Send ajax request over to the DB and print response of the RESTDB storage to console.
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
 };
+
 function decodeJwtResponse(token) {
   var base64Url = token.split(".")[1];
   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
