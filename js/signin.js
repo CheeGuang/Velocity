e1fb8d6cb6ef229f17491a041411440a06cea6d6
JeffreyLee <s10258143@connect.np.edu.sg>
2024-01-24 01:07:44 +0800

    Defined decodeJwtResponse funcition

--- js/signin.js
+++ js/signin.js
@@ -10,3 +10,17 @@ globalThis.handleCredentialResponse = async (response) => {
   console.log("Image URL: " + responsePayload.picture);
   console.log("Email: " + responsePayload.email);
 };
+function decodeJwtResponse(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
