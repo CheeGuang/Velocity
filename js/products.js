c1ed10edc4ce74ccd79ab3c723db2444877d7ad7
JeffreyLee <s10258143@connect.np.edu.sg>
2024-02-06 17:39:34 +0800

    Fixed redirectToProductDetails function

--- js/products.js
+++ js/products.js
@@ -146,6 +146,12 @@ function redirectToProductDetail(event) {
   // Encode the product name for further processing or sending to another page
   var encodedProductName = encodeURIComponent(productName);
 
+  // Construct the URL with query parameters
+  var redirectUrl = "productDetail.html" + "?name=" + encodedProductName;
+  console.log(productName);
+  console.log(encodedProductName);
+  console.log(redirectUrl);
+
   // Redirect to the product detail page with the query parameters
   window.location.href = redirectUrl;
 }
